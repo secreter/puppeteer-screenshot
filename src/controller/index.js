41af7844e9dc37a9b6909fb868cf1398442b0541
secreter <1979510177@qq.com>
2018-11-14 19:54:30 +0800

    finish get method

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,22 +2,70 @@
  * Created by pengchaoyang on 2018/11/9
  */
 const Screenshot = require('../lib/Screenshot');
-const screenshot=new Screenshot();
+const Boom = require('boom');
+const devices=require('../lib/DeviceDescriptors');
+const deviceNames=devices.map(device=>device.name)
+
+const instance=new Screenshot();
 (async ()=>{
-  await screenshot.launch()
+  await instance.launch()
 })();
 
 async function getMethod (ctx) {
-  ctx.type = 'image/png';
   let options={
-    url:ctx.query.url||'http://www.redream.cn',
-
+    url:ctx.query.url,
+    screenshot:{
+      type:ctx.query.type,
+      quality:ctx.query.quality,
+      fullPage:ctx.query.fullPage,
+      clip:{
+        x:ctx.query.x,
+        y:ctx.query.y,
+        width:ctx.query.w,
+        height:ctx.query.h,
+      },
+      omitBackground:ctx.query.o
+    },
+    device:ctx.query.device
   }
-  ctx.body=await screenshot.getImage(options)
+  if(!(options=checkOption(options,ctx))) return
+  ctx.type = options.screenshot.type;
+  ctx.body=await instance.getImage(options)
 }
 
 async function postMethod (ctx) {
-  ctx.body=await screenshot.getImage(options)
+  let options={}
+  if(!(options=checkOption(options,ctx))) return
+  ctx.type = options.screenshot.type;
+  ctx.body=await instance.getImage(options)
+}
+
+function checkOption(option,ctx){
+    let screenshot=option.screenshot
+    if(!screenshot.fullPage){
+        delete screenshot.clip
+    }
+    if(!/^https?:\/\/.+/.test(option.url)){
+        ctx.body=Boom.badRequest('invalid url').output;
+        return false
+    }
+    if(screenshot.type&&!/^jpeg$|^png$/.test(screenshot.type)){
+        ctx.body=Boom.badRequest('invalid type. Optional [jpeg,png]').output;
+        return false
+    }
+    if(screenshot.quality&&!Number.isInteger(screenshot.quality)){
+        ctx.body=Boom.badRequest(`quality is a number`).output;
+        return false
+    }
+    if(option.device&&deviceNames.indexOf(option.device)===-1){
+        ctx.body=Boom.badRequest(`invalid device. Optional [${deviceNames}]`).output;
+        return false
+    }else{
+        option.device=devices[option.device]
+    }
+
+
+    return option
 }
 
 module.exports={
